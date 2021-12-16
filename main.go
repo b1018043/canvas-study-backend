bf9016713ec711f485eef366ff395677c7dc82ff
b1018043 <grey.fox.0417@gmail.com>
2021-12-17 00:37:50 +0900

    feat:入退室時の処理の追加

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
 	"gopkg.in/olahol/melody.v1"
 )
 
+type WsResponse struct {
+	Type string `json:"type"`
+	ID   uint64 `json:"id"`
+	X    int64  `json:"x"`
+	Y    int64  `json:"y"`
+}
+
 func main() {
 	r := gin.Default()
 	m := melody.New()
+
+	var id uint64 = 0
+
+	clientid := make(map[*melody.Session]uint64)
+
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(400, gin.H{
 			"mes": "ok",
@@ -18,8 +32,35 @@ func main() {
 		m.HandleRequest(ctx.Writer, ctx.Request)
 	})
 
+	m.HandleConnect(func(s *melody.Session) {
+		clientid[s] = id
+		id++
+		wr := &WsResponse{
+			Type: "join",
+			ID:   clientid[s],
+		}
+		if bytes, err := json.Marshal(wr); err != nil {
+			// Nothing to do
+		} else {
+			m.BroadcastOthers(bytes, s)
+		}
+	})
+
+	m.HandleDisconnect(func(s *melody.Session) {
+		wr := &WsResponse{
+			Type: "leave",
+			ID:   clientid[s],
+		}
+		if bytes, err := json.Marshal(wr); err != nil {
+			// Nothing to do
+		} else {
+			m.BroadcastOthers(bytes, s)
+			delete(clientid, s)
+		}
+	})
+
 	m.HandleMessage(func(s *melody.Session, msg []byte) {
-		m.Broadcast(msg)
+		m.BroadcastOthers(msg, s)
 	})
 
 	r.Run(":8080")
